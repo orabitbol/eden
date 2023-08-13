8f96766db35bc04a9b8da7d9f4cd95eb68c06497
orabitbol <orabitbol@gmail.com>
2023-08-13 16:15:55 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
-import './App.scss'
-import Logo from './assets/logo.jpeg'
-import Picture from './assets/pic_eden.jpeg'
+import { useState } from "react";
+import "./App.scss";
+import Logo from "./assets/logo.jpeg";
+import Picture from "./assets/pic_eden.jpeg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AboutMe from './component/aboutMe/AboutMe';
-import Explanation from './component/explanation/Explanation';
-import Contact from './component/contact/Contact';
-import Home from './component/home/Home';
+import AboutMe from "./component/aboutMe/AboutMe";
+import Explanation from "./component/explanation/Explanation";
+import Contact from "./component/contact/Contact";
+import Home from "./component/home/Home";
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-
     <Routes>
       <Route index element={<Home />} />
       <Route path="aboutme" element={<AboutMe />} />
       <Route path="explanation" element={<Explanation />} />
       <Route path="contact" element={<Contact />} />
     </Routes>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,68 +1,77 @@
-import React from 'react'
-
-import Me from '../../assets/pic_eden.jpeg'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Me from "../../assets/pic_eden.jpeg";
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    // Replace '1234567890' with the phone number you want to chat with
+    const phoneNumber = "0528383680";
 
-    const handleClick = () => {
-        // Replace '1234567890' with the phone number you want to chat with
-        const phoneNumber = '0528383680';
+    // Construct the WhatsApp API URL with the phone number
+    const whatsappUrl = `https://wa.me/${phoneNumber}`;
 
-        // Construct the WhatsApp API URL with the phone number
-        const whatsappUrl = `https://wa.me/${phoneNumber}`;
-
-        // Open the WhatsApp URL in a new tab
-        window.open(whatsappUrl, '_blank');
-    };
-    return (
-        <div style={{ height: "900px", background: "#eee4e2" }}>
-            <div className='home'>
-                <div className='wapper-picture'>
-                    <img className='picture' src={Me} />
-                </div>
-
-                <div className='social-media'>
-                    <a
-                        className='social-text'
-                        href="https://www.instagram.com/eden_azulayy_1/"
-                        target="_blank"
-                        style={{ color: 'black', fontSize: '20px' }}
-                    >
-                        Instagram
-                    </a>
-                </div>
-
-                <div className='option'>
-                    <div className='top'>
-                        <div className='move-explanation'>
-                            <a
-                                href='explanation'
-                                style={{ color: 'black', textDecoration: 'none', fontSize: '20px' }}
-                            >  ?מהי נומרולוגיה </a>
-                        </div>
-                        <div className='move-about-me' >
-                            <a
-                                href='aboutme'
-                                style={{ color: 'black', textDecoration: 'none', fontSize: '20px' }}
-                            >   על עצמי</a>
-                        </div>
-
-                    </div>
-                    <div className='bottom'>
-                        <div
-                            className='move-contact'
-                            style={{ color: 'black', fontSize: '20px' }}
-                        >
-                            <div onClick={handleClick}>
-                                צור קשר
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+    // Open the WhatsApp URL in a new tab
+    window.open(whatsappUrl, "_blank");
+  };
+  return (
+    <div style={{ height: "900px", background: "#eee4e2" }}>
+      <div className="home">
+        <div className="wapper-picture">
+          <img className="picture" src={Me} />
         </div>
-    )
-}
 
-export default Home
+        <div className="social-media">
+          <a
+            className="social-text"
+            href="https://www.instagram.com/eden_azulayy_1/"
+            target="_blank"
+            style={{ color: "black", fontSize: "20px" }}
+          >
+            Instagram
+          </a>
+        </div>
+
+        <div className="option">
+          <div className="top">
+            <div className="move-explanation">
+              <div
+                onClick={() => navigate("/explanation")}
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  fontSize: "20px",
+                }}
+              >
+                {" "}
+                ?מהי נומרולוגיה{" "}
+              </div>
+            </div>
+            <div className="move-about-me">
+              <div
+                onClick={() => navigate("/aboutme")}
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  fontSize: "20px",
+                }}
+              >
+                {" "}
+                על עצמי
+              </div>
+            </div>
+          </div>
+          <div className="bottom">
+            <div
+              className="move-contact"
+              style={{ color: "black", fontSize: "20px" }}
+            >
+              <div onClick={handleClick}>צור קשר</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
